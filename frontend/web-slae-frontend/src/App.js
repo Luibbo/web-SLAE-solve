124f8745a34d80402b61c19b543c69f2d95ba348
Luibbo <lubomyrbotckiv@gmail.com>
2025-11-10 15:52:22 +0200

    add redirect to login

--- frontend/web-slae-frontend/src/App.js
+++ frontend/web-slae-frontend/src/App.js
@@ -5,34 +5,61 @@ import TaskHistory from "./Components/TaskHistory/TaskHistory";
 import CreateTask from "./Components/CreateTask/CreateTask";
 
 function App() {
+  
+  function LoginRedirectWrapper() {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+      const token = localStorage.getItem("token");
+      if (token) {
+        navigate("/tasks");
+        }
+      }, [navigate]);
+
+    return <LoginSignup/>;
+  }
+
+    function HistoryRedirectWrapper() {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+      const token = localStorage.getItem("token");
+      if (!token) {
+        navigate("/login");
+        }
+      }, [navigate]);
+
+    return <TaskHistory/>;
+  }
+
+  function CreateRedirectWrapper() {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+      const token = localStorage.getItem("token");
+      if (!token) {
+        navigate("/login");
+        }
+      }, [navigate]);
+
+    return <CreateTask/>;
+  }
+
 return (
 <Router>
   <Routes>
     <Route path="/login" element={<LoginRedirectWrapper />} />
 
-    <Route path="/tasks" element={<TaskHistory />} />
+    <Route path="/tasks" element={<HistoryRedirectWrapper />} />
 
-    <Route path="/create" element={<CreateTask />} />
+    <Route path="/create" element={<CreateRedirectWrapper />} />
 
     <Route path="*" element={<Navigate to="/login" />} />
   </Routes>
 </Router>
-
-
 );
 }
 
-function LoginRedirectWrapper() {
-const navigate = useNavigate();
 
-useEffect(() => {
-  const token = localStorage.getItem("token");
-  if (token) {
-    navigate("/tasks");
-    }
-  }, [navigate]);
-
-  return <LoginSignup/>;
-}
 
 export default App;
--- frontend/web-slae-frontend/src/api.js
+++ frontend/web-slae-frontend/src/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = process.env.REACT_APP_API_URL || "https://localhost/api/v1";
+const BASE_URL = "https://localhost/api/v1";
 
 function getAuthHeaders() {
   const token = localStorage.getItem("token");
